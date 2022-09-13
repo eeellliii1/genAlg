a8dcd4a8942b064ebd05bf59e6a053caf7c856af
unknown <earao@hotmail.com>
2022-09-13 15:13:50 -0700

    Created new refactor branch to refactor code to be general and modular

---
+++ games.py
@@ -0,0 +1,4 @@
+## make it so then people can add custom games to be trained here
+### 1: needs the type of neural net and how the game state is inputted
+### 2: needs a play method which will play the game between the current and given player (or other method of 1v1)
+### 3: needs a advanced version to strive towards (optional hunter killer such as minimax to encourage strong growth)
